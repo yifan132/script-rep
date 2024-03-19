3d25c880ca202b15e14adef2ae6138aae78f28de
yifan132 <yangyf132@hotmail.com>
2024-03-19 22:19:08 +0800

    20240319学习

--- python/2.程序的流程控制/5.if嵌套.py
+++ python/2.程序的流程控制/5.if嵌套.py
@@ -5,7 +5,7 @@ if answer =='是':
     proof=eval(input('酒精度数：'))
     if proof < 20:
         print('不构成酒驾')
-    if 20<=proof<80:
+    if 20<=proof<=80:
         print('构成酒后驾驶')
     if proof>80:
         print('构成醉驾')
--- test/test1.py
+++ test/test1.py
@@ -1,9 +1,17 @@
-import Numpy as np
-
-def sigmoid(x):
-    return 1 / (1 + np.exp(-x))
-
-# 计算Sigmoid函数值
-x = 0
-result = sigmoid(x)
-print(result)
+ #直角三角形
+j=1
+for i in range(1,6):
+    for j in range(1,7-i):
+        print('*',end='')
+    print() 
+    
+    
+    
+j=1
+for i in range(1,6):
+    for j in range(1,7-i):
+        print('*',end='')
+    print() 
+    
+    
+    
